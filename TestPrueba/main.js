5e8d33d841bb55e9062caa9fcac284d0f96cb339
eld13g0 <82134232+eld13g0@users.noreply.github.com>
2021-04-15 18:33:57 -0400

    Add files via upload

---
+++ TestPrueba/main.js
@@ -0,0 +1,6 @@
+
+ScrollReveal().reveal('.contenedor-info', { delay: 500 });
+ScrollReveal().reveal('.contenedor-casas', { delay: 500 });
+ScrollReveal().reveal('.contenedor-decoracion', { delay: 500 });
+ScrollReveal().reveal('.mapa', { delay: 500 });
+ScrollReveal().reveal('.contenedor-footer', { delay: 500 });
